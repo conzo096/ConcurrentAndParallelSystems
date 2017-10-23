5c2f6235a3cbdf974034b744e25749c825490d87
Conner Weatherston <40167111@live.napier.ac.uk>
2017-10-23 16:43:33 +0100

    Sampled 4096 dim size.

--- CourseworkPartOne/CourseworkPartOne/Main.cpp
+++ CourseworkPartOne/CourseworkPartOne/Main.cpp
@@ -292,7 +292,7 @@ bool array2bmp(const std::string &filename, const vector<vec> &pixels, const siz
 int main(int argc, char **argv)
 {
 	ofstream file;
-	file.open("Samples(20).txt");
+	file.open("Dim(4096).txt");
 	for (int i = 0; i < 100; i++)
 	{
 		clock_t t;
@@ -303,8 +303,8 @@ int main(int argc, char **argv)
 		auto get_random_number = bind(distribution, generator);
 
 		// *** These parameters can be manipulated in the algorithm to modify work undertaken ***
-		constexpr size_t dimension = 1024;
-		constexpr size_t samples = 5; // Algorithm performs 4 * samples per pixel.
+		constexpr size_t dimension = 4096;
+		constexpr size_t samples = 1; // Algorithm performs 4 * samples per pixel.
 		vector<sphere> spheres
 		{
 			sphere(1e5, vec(1e5 + 1, 40.8, 81.6), vec(), vec(0.75, 0.25, 0.25), reflection_type::DIFFUSE),
